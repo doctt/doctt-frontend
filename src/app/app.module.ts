e983b48a65c34f759490e183c8cb65362affb165
Denys Vitali <denys@denv.it>
2019-03-05 14:37:29 +0100

    Improve app layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatButtonModule, 
-    MatMenuModule,
-    MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+} from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './shared/components/home/home.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./shared/components/home/home.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
-import '../styles/theme.scss';
-
+import "../styles/theme.scss";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        FooterComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        BrowserModule,
-        AppRoutingModule,
-        MatButtonModule, 
-        MatMenuModule,
-        MatCheckboxModule
-    ],
-    providers: [],
-    bootstrap: [
-        AppComponent
-    ]
+  declarations: [AppComponent, HomeComponent, FooterComponent],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule {}
