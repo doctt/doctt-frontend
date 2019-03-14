bf5c83b3e5847369be8e82faeb80ad700fb8a7ef
Cristian Spozio <cristian.spozio@student.supsi.ch>
2019-03-14 11:52:58 +0100

    Add xml file data storing

--- src/app/shared/components/xmlupload/xmlupload.component.ts
+++ src/app/shared/components/xmlupload/xmlupload.component.ts
@@ -1,22 +1,18 @@
 import { Component } from "@angular/core";
 import { HtmlParser } from "@angular/compiler";
+import { parse } from "url";
 
-let debug = 1;
-
-interface FileInfo {
-  version: String;
-  encoding: String;
-}
+let debug = 0;
 
 interface Header {
-  textfile: String;
-  lang: String;
+  textfile: string;
+  lang: string;
 }
 
 interface Segment {
-  features: String[];
-  id: BigInteger;
-  state: String;
+  features: string[];
+  id: number;
+  state: string;
 }
 
 interface Body {
@@ -29,8 +25,22 @@ interface Document {
 }
 
 interface File {
-  info: FileInfo;
+  version : number;
   data: Document;
+}
+
+function createSegment(features: string[], id: number, state: string) {
+  let newSegment = { features : [""] , id: -1, state : "active"};
+  if (features) {
+    newSegment.features = features;
+  }
+  if(id){
+    newSegment.id = id;
+  }
+  if(state){
+    newSegment.state = state;
+  }
+  return newSegment;
 }
 
 @Component({
@@ -50,29 +60,69 @@ export class XmlUploadComponent {
         console.log(e);
         console.log(fileReader.readyState);
         console.log(fileReader.result.toString());
-        let v = new DOMParser().parseFromString(
-          fileReader.result.toString(),
-          "text/xml"
-        );
-        
-        let r = Array.prototype.map.call(
-          v.querySelector("body").childNodes,
-          (a: Node, i : Number) => {
-            if (a.nodeType === Node.TEXT_NODE) {
-               let d = document.createElement("segment");
-               d.innerHTML = a.textContent;
-               a = d;
-            }
-            
-            (<HTMLElement> a).setAttribute("id", i.toString());
-            return a;
+      }
+      let parser = new DOMParser().parseFromString(
+        fileReader.result.toString(),
+        "text/xml"
+      );
+      
+      let segmentsArray : Array<HTMLElement> = Array.prototype.map.call(
+        parser.querySelector("body").childNodes,
+        (a: Node, i : Number) => {
+          if (a.nodeType === Node.TEXT_NODE) {
+             let d = document.createElement("segment");
+             d.innerHTML = a.textContent;
+             a = d;
           }
-        );
+          
+          (<HTMLElement> a).setAttribute("id", i.toString());
+          return a;
+        }
+      );
+    
+      if(debug){
+        console.log(segmentsArray[0].textContent.split('\n'));
+        console.log(segmentsArray);
+      }
 
-        console.log(r[0].textContent.split('\n'));
-        debugger;
+      let segs : Segment[] = [];
+      for(let i in segmentsArray){
+        let features_str : string;
+        features_str = segmentsArray[i].getAttribute("features");
+        let features : string[];
+        if(features_str)
+          features = features_str.split(";");
+          segs.push(createSegment(
+          features,
+          parseInt(segmentsArray[i].getAttribute("id")),
+          "active"
+        ));
+      }
 
-        console.log(r);
+      if(debug){
+        for(let i in segs){
+          console.log(i, "\t ",  segs[i].features);
+        }
+      }
+
+      let headerArray = parser.querySelector("header").children;
+      let head : Header = {
+        textfile : headerArray[0].textContent, 
+        lang : headerArray[1].textContent
+      };
+
+      let bod : Body = {
+        segments : segs
+      }
+
+      let doc : Document = {
+        header : head,
+        body : bod
+      }
+
+      let file : File = {
+        version : 1,
+        data : doc
       }
     };
 
