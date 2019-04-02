a9e9a9dd66eb030fc12fd977bdef4df173e9fc73
Denys Vitali <denys@denv.it>
2019-04-02 12:56:08 +0200

    Use Jest w/ Angular

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'jest-preset-angular',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     "^Models/(.*)$": "<rootDir>/src/app/models/$1",
     "^Services/(.*)$": "<rootDir>/src/app/services/$1",
     "^Resources/(.*)$": "<rootDir>/resources/$1"
-  }
+  },
+  "setupFilesAfterEnv": [
+    "<rootDir>/src/setupJest.ts"
+  ]
 };
