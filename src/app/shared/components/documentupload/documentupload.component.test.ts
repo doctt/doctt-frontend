8ac8f8d4ac2dbe74be3256b193c9c99ff9a59df4
Denys Vitali <denys@denv.it>
2019-04-16 13:44:45 +0200

    Comment out test

--- src/app/shared/components/documentupload/documentupload.component.test.ts
+++ src/app/shared/components/documentupload/documentupload.component.test.ts
@@ -3,7 +3,7 @@ import { DocumentUploadComponent } from './documentupload.component';
 import { DocumentParserService } from 'Services/parser/document/DocumentParser';
 
 test("upload", () =>{
-  let docUpload : DocumentUploadComponent = new DocumentUploadComponent(new DocumentParserService);
+  //let docUpload : DocumentUploadComponent = new DocumentUploadComponent(new DocumentParserService);
   //let filename : Document = fs.readFileSync("resources/test/xml/filename.xml");
 
   //docUpload.onUpload(filename);
