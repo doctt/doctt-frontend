5d0c8746390b9403d6ab249e62969814b0452379
Denys Vitali <denys@denv.it>
2019-04-17 13:06:38 +0200

    DocumentParser: Fix test to reflect file changes

--- src/app/services/parser/document/DocumentParser.test.ts
+++ src/app/services/parser/document/DocumentParser.test.ts
@@ -55,7 +55,7 @@ test('parseXML2', () => {
     expect(file.data.header.lang).toBe("english");
     expect(file.data.body.segments.length).toBe(3);
     expect(file.data.body.segments[1].features)
-        .toEqual(["clauses", "verbal", "finite-clause", "nonmodal", "active-clause"]);
+        .toEqual(["clauses", "verbal"]);
     expect(file.data.body.segments[1].state).toBe("active");
     expect(file.data.body.segments[1].text).toEqual(
         "Greetings, and welcome to the Microsoft Fiscal Year 2019 First Quarter Earnings Conference Call. As a reminder, this conference is being recorded. It is now my pleasure to introduce your host, Mike Spencer, General Manager, Investor Relations. Thank you. You may begin."
