c9f947fe89d0b4a5b8c77cf677ddee2d8e4056a1
Denys Vitali <denys@denv.it>
2019-05-16 13:53:49 +0200

    Fix colors

--- src/app/services/tree/TreeColorizer.ts
+++ src/app/services/tree/TreeColorizer.ts
@@ -1,21 +1,21 @@
-import { HSLColor } from "Models/hslcolor/HSLColor";
-import { ColorizedNode } from "Models/tree/ColorizedTree";
-import { TreeNode } from "Models/tree/Tree";
+import { HSLColor } from 'Models/hslcolor/HSLColor';
+import { ColorizedNode } from 'Models/tree/ColorizedTree';
+import { TreeNode } from 'Models/tree/Tree';
 
 class TreeColorizer {
   private static colors: Array<HSLColor> = [
-    HSLColor.toHSL("#9E9E9E"),
-    HSLColor.toHSL("#f44336"),
-    HSLColor.toHSL("#673AB7"),
-    HSLColor.toHSL("#4CAF50"),
-    HSLColor.toHSL("#FFC107"),
-    HSLColor.toHSL("#009688"),
-    HSLColor.toHSL("#795548"),
-    HSLColor.toHSL("#E91E63"),
+    HSLColor.toHSL('#ECEFF1'),
+    HSLColor.toHSL('#f44336'),
+    HSLColor.toHSL('#673AB7'),
+    HSLColor.toHSL('#4CAF50'),
+    HSLColor.toHSL('#FFC107'),
+    HSLColor.toHSL('#009688'),
+    HSLColor.toHSL('#795548'),
+    HSLColor.toHSL('#E91E63'),
   ];
 
   static colorize(root: TreeNode): ColorizedNode[] {
-    let nodes: ColorizedNode[] = [];
+    const nodes: ColorizedNode[] = [];
     nodes.push(this.getColorizedNode(root, this.colors[0], -1, 0));
 
     return nodes;
@@ -28,18 +28,18 @@ class TreeColorizer {
     element: number
   ): ColorizedNode {
     let newColor: HSLColor;
-    if (level == -1) {
+    if (level === -1) {
       // Root
       newColor = this.colors[0];
       newColor.s = 50;
       newColor.l = 50;
-    } else if (level == 0) {
-      let c = this.colors[element + 1];
+    } else if (level === 0) {
+      const c = this.colors[element + 1];
       newColor = new HSLColor(c.h, 60, 44);
     } else {
       newColor = this.getColor(color, level, element);
     }
-    let dfn: ColorizedNode = {
+    const dfn: ColorizedNode = {
       name: node.id,
       color:
         level == 0 || level == -1
@@ -81,12 +81,12 @@ class TreeColorizer {
   }
 
   static getColor(color: HSLColor, level: number, element: number): HSLColor {
-    let nc = new HSLColor(color.h, color.s, color.l);
+    const nc = new HSLColor(color.h, color.s, color.l);
 
     nc.h += 4 + Math.exp(-level) * 15 * element;
     nc.l += 8;
     nc.s += 10;
-    //nc.h = nc.h % 360;
+    // nc.h = nc.h % 360;
 
     return nc;
   }
